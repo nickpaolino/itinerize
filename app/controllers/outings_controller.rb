0b857170b395078c3fe8ed6a4e19d378c6dac009
Nick Paolino <github email address>
2017-10-24 11:41:02 -0400

    Finished create action for outing

--- app/controllers/outings_controller.rb
+++ app/controllers/outings_controller.rb
@@ -8,13 +8,25 @@ class OutingsController < ApplicationController
   end
 
   def create
-    byebug
+    @outing = Outing.new(outing_params)
+
+    @outing.save
+
+    redirect_to outing_path(@outing)
   end
 
   def show
+    @outing = Outing.find(params[:id])
+
     @user = User.find(session[:user_id])
 
     # Filters out the current user and creates a list of users for the create new outing page
     @users = User.all.select {|user| user.id != session[:user_id]}.map {|user| [user.username, user.id]}
+  end
+
+  private
+
+  def outing_params
+    params.require(:outing).permit(:name, :event_start, :voting_deadline)
   end
 end
