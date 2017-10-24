27e206b490891d6757842d2685a6cfbed5bfcd82
Nick Paolino <github email address>
2017-10-24 19:54:41 -0400

    Updated Time Functions

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -2,4 +2,13 @@ class Outing < ApplicationRecord
   has_many :suggestions
   has_many :user_outings
   has_many :users, through: :user_outings
+
+  def time_left
+    time = (self.voting_deadline - Time.now).to_i.abs
+    hours = time / 3600
+    time = time - (hours * 3600)
+    minutes = time / 60
+    time =- time - (minutes * 60)
+    "#{hours} hours #{minutes} minutes"
+  end
 end
