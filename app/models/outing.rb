5f2c68bcfc2594b84a7bc33114f1e9f6bab40609
Nick Paolino <github email address>
2017-10-24 20:25:32 -0400

    Got the timer and voting_over? method to work

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -3,12 +3,22 @@ class Outing < ApplicationRecord
   has_many :user_outings
   has_many :users, through: :user_outings
 
-  def time_left
-    time = (self.voting_deadline - Time.now).to_i.abs
+  def timer_formatted
+    time = time_left.abs
     hours = time / 3600
     time = time - (hours * 3600)
     minutes = time / 60
     time =- time - (minutes * 60)
     "#{hours} hours #{minutes} minutes"
+  end
+
+  def voting_over?
+    time_left <= 0
+  end
+
+  private
+
+  def time_left
+    time = (self.voting_deadline - Time.now).to_i
   end
 end
